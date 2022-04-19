8c9fe4810663c4aa87c0f78c6be879292cdee4c1
Username <daniellesmikle@Danielles-Air.home>
2022-04-18 23:44:40 -0400

    starting to seed

---
+++ db/WorkOut-seeds.js
@@ -0,0 +1,5 @@
+const WorkOut = require('../models/WorkOut-model')
+const seedData = require('./WorkOut-seed.js')
+
+
+console.log(seedData)
---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const mongoURI = 
+    process.env.NODE_ENV === 'production'
+    ? process.env.DB_URL
+    : "mongodb+srv://test:test@cluster0.qctpu.mongodb.net/GymApp?retryWrites=true&w=majority"
+
+    mongoose.connect(mongoURI)
+        .then(instance => console.log(`Connected to: ${instance.connections[0].name}`)
+        )
+        .catch(error => console.log(`failed conn:`, error))
+
+    module.exports = mongoose
