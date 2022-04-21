3cf30a5ccbfe43081c5d7c26a08fdbb12b535f37
Username <daniellesmikle@Danielles-Air.home>
2022-04-20 20:23:17 -0400

    GymApp

--- db/WorkOut-seeds.js
+++ db/WorkOut-seeds.js
@@ -1,5 +1,18 @@
 const WorkOut = require('../models/WorkOut-model')
-const seedData = require('./WorkOut-seed.js')
+const seedData = require('./WorkOut-seeds.json')
 
 
 console.log(seedData)
+
+
+
+WorkOut.deleteMany({})
+    .then(()=> {
+        return WorkOut.insertMany(seedData)
+    })
+    .then(console.log)
+    .catch(console.error)
+    .finally( () => {
+
+        process.exit()
+    })
---
+++ models/WorkOut-model.js
@@ -0,0 +1,22 @@
+const { type } = require('express/lib/response');
+const mongoose = require('../db/connection')
+// const ExcerciseNameSchema = new mongoose.Schema({ name: String });
+const WorkOutSchema = new mongoose.Schema(
+    {
+        label: {type: String, required: true},
+          
+        excercise: {          
+          name: [String],
+          sets: Number,
+          description: String
+        },
+        date: String
+         
+    }
+      
+    );
+    
+
+const WorkOut = mongoose.model('WorkOut', WorkOutSchema)
+
+module.exports = WorkOut;
